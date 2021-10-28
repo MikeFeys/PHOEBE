7e10dc60774ce3857f1d6e63b49780b614c813f1
Mike <mike.feys@kuleuven.be>
2021-10-28 16:42:13 +0200

    Update

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-import phoebe
-b=phoebe.default_binary()
-b.set_value('teff', component='secondary', value=5000)
-b.set_value('q', value=0.75)
-b.add_dataset('lc', times=phoebe.linspace(0,2,101))
-b.add_dataset('rv', times=phoebe.linspace(0,2,101))
-b.run_compute()
-b.plot(x='phases', show=True)
-b.show()
+import scipy.signal as signal
+import numpy as np
+import matplotlib.pyplot as plt
+
+RVpath = './data22_RV_info.dat'
+LCpath = './data22_OGLE.dat'
+
+HJD=[]
+I_band=[]
+I_Err=[]
+with open(LCpath) as f:
+    for line in f:
+        temp=line.split()
+        HJD.append(temp[0])
+        I_band.append(temp[1])
+        I_Err.append(temp[2])
+print(I_band)
+#atom-python-run en dan f5
+frequencies_Lombscargle = np.linspace(0.01, 100,10000)
