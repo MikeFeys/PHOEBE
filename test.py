8b70ad28a24d3b9422c7610760a05618546ab87f
Mike <mike.feys@kuleuven.be>
2021-10-28 16:57:24 +0200

    Update

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import scipy.signal as signal
+from astropy.timeseries import LombScargle
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,9 +11,14 @@ I_Err=[]
 with open(LCpath) as f:
     for line in f:
         temp=line.split()
-        HJD.append(temp[0])
-        I_band.append(temp[1])
-        I_Err.append(temp[2])
-print(I_band)
+        HJD.append(float(temp[0]))
+        I_band.append(float(temp[1]))
+        I_Err.append(float(temp[2]))
 #atom-python-run en dan f5
 frequencies_Lombscargle = np.linspace(0.01, 100,10000)
+frequency, power = LombScargle(HJD, I_band, I_Err).autopower(minimum_frequency=0.1, maximum_frequency=100)
+#false alarm levels
+probabilities = [0.1,0.05,0.01]
+FA_levels = LombScargle(HJD, I_band, I_Err).false_alarm_level(probabilities)
+plt.plot(frequency, power)
+plt.show()
