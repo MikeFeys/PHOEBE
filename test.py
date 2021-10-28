0efe2ef290d0a29deedf6c8ee1e75f6749440905
Mike <mike.feys@kuleuven.be>
2021-10-28 17:03:21 +0200

    Update

--- test.py
+++ test.py
@@ -19,6 +19,9 @@ frequencies_Lombscargle = np.linspace(0.01, 100,10000)
 frequency, power = LombScargle(HJD, I_band, I_Err).autopower(minimum_frequency=0.1, maximum_frequency=100)
 #false alarm levels
 probabilities = [0.1,0.05,0.01]
-FA_levels = LombScargle(HJD, I_band, I_Err).false_alarm_level(probabilities)
+FA_levels = LombScargle(HJD, I_band, I_Err).false_alarm_level(probabilities, method='bootstrap')
 plt.plot(frequency, power)
+plt.hlines(FA_levels[0],0.1,100,colors = ['red'],linestyle = 'dashed')
+plt.hlines(FA_levels[1],0.1,100,colors = ['orange'],linestyle = 'dashed')
+plt.hlines(FA_levels[2],0.1,100,colors = ['green'],linestyle = 'dashed')
 plt.show()
