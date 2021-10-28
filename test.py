21278784cf9716618db38d9d529bd7088a39dfbf
Mike <mike.feys@kuleuven.be>
2021-10-28 18:16:39 +0200

    Update

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ RVpath = './data22_RV_info.dat'
 LCpath = './data22_OGLE.dat'
 # initialize data arrays
 HJD=[] # heliocentric julian date
-I_band=[] # I-band
+I_band=[] # I-band magnitudes
 I_Err=[] # error in magnitudes
 #read lc data
 with open(LCpath) as f:
@@ -15,11 +15,17 @@ with open(LCpath) as f:
         HJD.append(float(temp[0]))
         I_band.append(float(temp[1]))
         I_Err.append(float(temp[2]))
+
+#conversion mag to flux: Magnitude = 22.5 - 2.5log_10(flux) so flux=10**((Magnitude-22.5)/-2.5)
+
 #plot raw lc data
 fig0 = plt.figure()
 ax0 = plt.gca()
 ax0.errorbar(HJD, I_band, I_Err, fmt='o')
-plt.show()
+plt.xlabel('HJD [d]')
+plt.ylabel('I band magnetude')
+plt.title('Light curve data of I band vs heliocentric julian date')
+
 #maximum and minimum time differences for Nyquist frequency
 maxtime=HJD[-1]-HJD[0]
 mintime = np.min(np.diff(HJD))
@@ -29,14 +35,17 @@ frequency, power = LombScargle(HJD, I_band, I_Err).autopower(samples_per_peak=10
 probabilities = [0.1,0.05,0.01]
 FA_levels = LombScargle(HJD, I_band, I_Err).false_alarm_level(probabilities, method='bootstrap')
 #plot lombscargle periodogram
+plt.figure()
 plt.plot(1/frequency, power)
 plt.hlines(FA_levels[0], maxtime/2,mintime/2,colors = ['red'],linestyle = 'dashed')
 plt.hlines(FA_levels[1],maxtime/2,mintime/2,colors = ['orange'],linestyle = 'dashed')
 plt.hlines(FA_levels[2],maxtime/2,mintime/2,colors = ['green'],linestyle = 'dashed')
+plt.text(maxtime/2,0.95*FA_levels[0],'10%',color='red')
+plt.text(maxtime/2,FA_levels[1],'5%',color='orange')
+plt.text(maxtime/2,FA_levels[2],'1%',color='green')
+plt.xlabel('Period [days]')
+plt.ylabel('Power [no unit/normalized]')
+plt.title('Lombscargle periodogram using bootstrap method for false alarms.')
 plt.show()
-#extract the period associated to the absolute maximum in power
-max=np.max(power)
-index=np.where(power == max)[0][0]
-period = frequency[index]
-print(period)
+#extract the frequency associated to the absolute maximum in power
 print(frequency[np.where(power== np.amax(power))])
