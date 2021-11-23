7dc8e13ebfb63bc654e77e86acec462e483f52bc
lukasvin <lukas@vinoelst.com>
2021-11-23 15:38:54 +0100

    updating my version

--- fold.py
+++ fold.py
@@ -7,6 +7,10 @@ RVpath = './data22_RV_info.dat'
 LCpath = './data22_OGLE.dat'
 period_cand = [1,795,663,428,1060] #in days
 
+def to_flux(mag_array):
+    flux_array = np.power(10*np.ones(len(mag_array)),(0.0356-mag_array)/2.5)
+    return flux_array
+
 #read data
 HJD=[] # heliocentric julian date
 I_band=[] # I-band magnitudes
@@ -18,13 +22,20 @@ with open(LCpath) as f:
         HJD.append(float(temp[0]))
         I_band.append(float(temp[1]))
         I_err.append(float(temp[2]))
+
+'''
+I_band = to_flux(np.array(I_band))
+I_err = to_flux(np.array(I_err))
+#I_err = np.zeros(len(I_band))
+'''
+
 for i in range(len(period_cand)):
     b = phoebe.default_binary()
 
     b.set_value('latex_repr', component='binary', value = 'orb')
     b.set_value('latex_repr', component='primary', value = '1')
     b.set_value('latex_repr', component='secondary', value = '2')
-    b.set_value('period@binary', period_cand[i])
+    b.set_value('period@binary', period_cand[i]*u.d)
 
     b.add_dataset('lc',
                 compute_phases=phoebe.linspace(0,1,201),
--- test.py
+++ test.py
@@ -12,12 +12,14 @@ I_Err=[] # error in magnitudes
 with open(LCpath) as f:
     for line in f:
         temp=line.split()
-        HJD.append(float(temp[0]))
-        I_band.append(float(temp[1]))
-        I_Err.append(float(temp[2]))
-
+        HJD.append(np.float(temp[0]))
+        I_band.append(np.float(temp[1]))
+        I_Err.append(np.float(temp[2]))
+'''
 #conversion mag to flux: Magnitude = 22.5 - 2.5log_10(flux) so flux=10**((Magnitude-22.5)/-2.5)
-
+I_flux = np.divide((I_band-np.ones(len(I_band))*22.5),-2.5)
+I_flux_err = (I_Err-np.ones(len(I_band))*22.5)/-2.5
+'''
 #plot raw lc data
 fig0 = plt.figure()
 ax0 = plt.gca()
@@ -25,7 +27,14 @@ ax0.errorbar(HJD, I_band, I_Err, fmt='o')
 plt.xlabel('HJD [d]')
 plt.ylabel('I band magnetude')
 plt.title('Light curve data of I band vs heliocentric julian date')
-
+'''
+fig1 = plt.figure()
+ax1 = plt.gca()
+ax1.errorbar(HJD, I_flux, I_flux_err, fmt='o')
+plt.xlabel('HJD [d]')
+plt.ylabel('I band Flux')
+plt.title('Flux of I band vs heliocentric julian date')
+'''
 #maximum and minimum time differences for Nyquist frequency
 maxtime=HJD[-1]-HJD[0]
 mintime = np.min(np.diff(HJD))
@@ -34,6 +43,17 @@ frequency, power = LombScargle(HJD, I_band, I_Err).autopower(samples_per_peak=10
 #false alarm levels
 probabilities = [0.1,0.05,0.01]
 FA_levels = LombScargle(HJD, I_band, I_Err).false_alarm_level(probabilities, method='bootstrap')
+#look for best frequencies
+bestfreq = []
+bestind = []
+search_freq = frequency
+search_pow = power
+for i in range(5):
+    bestind = np.argmax(search_pow)
+    bestfreq = search_freq[bestind]
+    search_freq = np.delete(search_freq,bestind)
+    search_pow = np.delete(search_pow,bestind)
+print(bestfreq)
 #plot lombscargle periodogram
 plt.figure()
 plt.plot(1/frequency, power)
